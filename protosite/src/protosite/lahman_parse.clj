6a22882193eec6da7e24a7d16c5b51076af38034
Matthew Rocklin <mrocklin@gmail.com>
2013-07-29 10:34:04 -0500

    add file to parse lahman Master file

---
+++ protosite/src/protosite/lahman_parse.clj
@@ -0,0 +1,39 @@
+(ns protosite.lahman-parse
+  (:require [clojure.data.csv :as csv])
+  (:require [clojure.java.io :as io]))
+
+(def text (with-open [in-file (io/reader "resources/lahman2012/Master.csv")]
+              (doall (csv/read-csv in-file))))
+
+(def maps (let [[header & data] text]
+            (for [line data] (zipmap header line))))
+
+(defn parse-int [s]
+  (cond 
+    (instance? Number s) s
+    (instance? String s) (. Integer parseInt s)))
+
+(defn date-of [year month day] (read-string (apply format "#inst \"%d-%02d-%02d\"" (map parse-int [year month day]))))
+
+(defn date-of-str [s] (let [[month day year] (clojure.string/split s #"/")]
+                        (date-of year month day)))
+
+(defn lahman-keywordify [k] (keyword (str "lahman." k)))
+
+(def int-fields #{"lahmanID" "height" "weight"})
+(def del-fields #{"birthYear" "birthMonth" "birthDay" "deathYear" "deathMonth" "deathDay"})
+(defn map-to-fact [m]
+  (let [birth (let [[y m d] (map m ["birthYear" "birthMonth" "birthDay"])]
+                (if (some empty? [y m d]) nil (date-of y m d)))
+        death (let [[y m d] (map m ["deathYear" "deathMonth" "deathDay"])]
+                (if (some empty? [y m d]) nil (date-of y m d)))
+        debut (let [d (m "debut")] (if (empty? d) nil (date-of-str (m "debut"))))
+        final (let [d (m "finalGame")] (if (empty? d) nil (date-of-str (m "finalGame"))))
+        with-new (assoc m "birth" birth "death" death "debut" debut "finalGame" final)
+        without-old (apply dissoc with-new del-fields)]
+    (apply hash-map (flatten (for [[k v] without-old]
+              [(lahman-keywordify k) (if (and (contains? int-fields k) (not (empty? v))) (parse-int v) v)])))))
+
+(def player-facts (map map-to-fact maps))
+
+; (eq (count player-facts) (count maps))
---
+++ protosite/test/protosite/lahman_test.clj
@@ -0,0 +1,18 @@
+(ns protosite.lahman-test
+  (:require [midje.sweet :refer :all]
+            [protosite.lahman-parse :refer :all])
+  (:use [datomic.api :only [db q] :as d]))
+
+(fact "parse-int parses ints robustly"
+      (parse-int 5) => 5
+      (parse-int "05") => 5
+      (parse-int "5") => 5)
+
+(fact "date-of produces times for a variety of inputs"
+      (type (date-of 1939 8 5)) => java.util.Date
+      (type (date-of "1939" "08" "05")) => java.util.Date
+      (type (date-of "1939" "8" "5")) => java.util.Date)
+
+(fact "date-of-str produces times"
+      (type (date-of-str "09/30/2001")) => java.util.Date)
+
