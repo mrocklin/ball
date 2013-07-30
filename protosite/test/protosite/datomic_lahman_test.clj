5f2899b06ebd9530e3cc913ceaf1262321211e4a
Matthew Rocklin <mrocklin@gmail.com>
2013-07-29 19:57:35 -0500

    test basic add and query in lahman database

--- protosite/test/protosite/datomic_lahman_test.clj
+++ protosite/test/protosite/datomic_lahman_test.clj
@@ -1,12 +1,21 @@
 (ns protosite.datomic-lahman-test
   (:require [midje.sweet :refer :all]
             [protosite.datomic-test :refer :all]
-            [protosite.lahman-schema :refer :all])
+            [protosite.lahman-schema :refer :all]
+            [protosite.lahman-parse :refer :all])
   (:use [datomic.api :only [db q] :as d]))
 
-(fact "Schema is loaded and consistent"
+(defn add-id [m] 
+  (assoc m :db/id (d/tempid :db.part/user)))
+
+(fact "Database handles basic queries"
        (with-connection conn
           (d/transact conn schema)
-          (q '[:find ?typ :where [?entity :db/ident :lahman.weight ]
-                                 [?entity :db/valueType ?typ       ]] (db conn))
-               => #{:db.type/int}))
+          (d/transact conn [
+              (-> {"AB" "5" "2B" "2" "playerID" "joe"} map-to-fact add-id)
+              (-> {"AB" "7" "2B" "3" "playerID" "sally"} map-to-fact add-id)])
+          (q '[:find ?AB :where [?ent :lahman/playerID "joe"]
+                                [?ent :lahman/AB ?AB]] (db conn))
+               => #{[5]}
+          (q '[:find ?player :where [?ent :lahman/playerID ?player]] (db conn))
+               => #{["joe"] ["sally"]}))
