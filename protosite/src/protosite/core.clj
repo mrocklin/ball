e902bac1ffb8c3c56c9b4323f8c5405b05f230b1
Matthew Rocklin <mrocklin@gmail.com>
2013-08-03 13:20:19 -0500

    ready-for-data-tables in core, also, port 5000

--- protosite/src/protosite/core.clj
+++ protosite/src/protosite/core.clj
@@ -2,13 +2,13 @@
   (:require [ring.adapter.jetty :refer :all])
   (:require [compojure.route :as route])
   (:require [compojure.core :refer :all])
-  (:require [protosite.models :refer [c-team-record]]))
+  (:require [protosite.models :refer :all]))
 
 
 (defroutes app
   (GET "/" [] "<h1>Welcome to Fantasy Baseball!</h1>")
   (GET ["/team/:team/:year" :team #"\w{3}" :year #"\d{4}"] [team year]
-         (str (c-team-record team (Integer/parseInt year))))
+         (ready-for-data-tables (c-team-record team (Integer/parseInt year))))
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
 
@@ -17,4 +17,4 @@
 
 (defn -main []
   (println "Server Started")
-  (run-jetty app {:port 3000}))
+  (run-jetty app {:port 5000}))
