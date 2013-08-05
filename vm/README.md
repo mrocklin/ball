6e68bca190bafe1acbd1f9566badbe94846f13ba
John Jacobsen <john@mail.npxdesigns.com>
2013-08-05 15:19:06 -0500

    First version of VM provisioning which works w/ both AWS and Virtualbox
    This may still be somewhat raw, but it works for me with both AWS and VBox.  YMMV.
    
    README.md has more info about how it works now.

--- vm/README.md
+++ vm/README.md
@@ -1,10 +1,23 @@
-Things you can do:
+You can create a local or AWS VM.
 
-"vagrant up" - create the VM
-"vagrant ssh" - log into the VM
+For a local VM,
+
+- `cd vbox`
+- `vagrant up`
+- `vagrant ssh`
+- (to refresh after changing Puppet manifests, `vagrant provision`).
+
+For an AWS VM,
+
+- `cd aws`
+- `vagrant up --provider=aws`.  For this to work, you'll want the following:
+    - `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` defined as environment variables
+    - `qball.pem` installed in `~/.ssh` with mode 0600.
+- `vagrant ssh`
+- `vagrant provision`  # as above
 
 On the VM:
 
-lein new foo
-cd foo
-lein repl
+    lein new foo
+    cd foo
+    lein repl
---
+++ vm/manifests/aws.pp
@@ -0,0 +1,57 @@
+$user = 'vagrant'
+
+group { $user:
+  ensure => present
+}
+
+file { "/home/$user":
+  ensure => directory,
+  owner => $user,
+  group => $user,
+  mode => '755',
+  require => User[$user]
+}
+
+user { $user:
+  ensure => present,
+  gid => $user,
+  groups => ["staff", "root"],
+  shell => "/bin/bash",
+  home => "/home/$user",
+  require => Group[$user]
+}
+
+file { "/home/$user/.ssh":
+  ensure => directory,
+  owner => $user,
+  group => $user,
+  mode => '700',
+  require => File["/home/$user"]
+}
+
+file { "/home/$user/.ssh/authorized_keys":
+  ensure => file,
+  owner => $user,
+  group => $user,
+  mode => '600',
+  content => 'INSERT_RSA_KEY_HERE',
+  require => File["/home/$user/.ssh"]
+}
+
+file { "/home/$user/.bash_profile":
+  ensure => file,
+  owner => $user,
+  group => $user,
+  mode => '644',
+  content => '# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
+fi
+
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi'
+}
