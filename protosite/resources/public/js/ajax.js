e6ab2b55090541941599d7ba88d380b2464d0e2b
Matthew Rocklin <mrocklin@gmail.com>
2013-08-09 17:36:39 -0500

    add fnServerData callback to ajax.js

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -7,7 +7,30 @@ $( document ).ready( function() {
     oTable = $('#example').dataTable({
         "bProcessing": true,
         "sAjaxSource": "/team/NYN/1990/",
-        "iDisplayLength": 25
+        "iDisplayLength": 25,
+		"fnServerData": function ( sUrl, aoData, fnCallback, oSettings ) {
+			oSettings.jqXHR = $.ajax( {
+				"url":  sUrl,
+				"data": aoData,
+				"success": function (json) {
+					if ( json.sError ) {
+						oSettings.oApi._fnLog( oSettings, 0, json.sError );
+					}
+					
+					$(oSettings.oInstance).trigger('xhr', [oSettings, json]);
+					fnCallback( json );
+				},
+				"dataType": "json",
+				"cache": true,
+				"type": "GET",
+				"error": function (xhr, error, thrown) {
+					if ( error == "parsererror" ) {
+						oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
+							"server could not be parsed. This is caused by a JSON formatting error." );
+					}
+				}
+			} );
+		},
     });
     console.log(oTable);
 
