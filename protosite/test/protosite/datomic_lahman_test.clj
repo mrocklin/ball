a8be678d000c1bcddb4dabb57cd4f4b8b5c8b230
Matthew Rocklin <mrocklin@gmail.com>
2013-08-10 13:22:10 -0500

    add datomic tests for team data

--- protosite/test/protosite/datomic_lahman_test.clj
+++ protosite/test/protosite/datomic_lahman_test.clj
@@ -27,17 +27,21 @@
 (def franchise-facts
   (datomic-facts-from-filename "resources/lahman2012/TeamsFranchises-test.csv"))
 
+(def teams-facts
+  (datomic-facts-from-filename "resources/lahman2012/Teams-test.csv"))
+
 (fact "Lahman dataset parses and is queryable"
       (with-connection conn
         (d/transact conn schema)
         (d/transact conn master-facts)
         (d/transact conn batting-facts)
         (d/transact conn franchise-facts)
+        (d/transact conn teams-facts)
         ; Dayrrl Strawberry was born in Los Angeles
         (q '[:find ?city :where [?x :lahman/playerID "strawda01"]
                                 [?x :lahman/birthCity ?city]] (db conn))
                        => #{["Los Angeles"]}
-        ; With what teams did Dayrrl play? 
+        ; With what teams did Dayrrl play?
         (q '[:find ?team :where [?x :lahman/playerID "strawda01"]
                                 [?x :lahman/teamID   ?team]] (db conn))
                        => #{["NYN"] ["LAN"] ["SFN"] ["NYA"]}
@@ -47,12 +51,22 @@
                                 [?x :lahman/birthCity "Los Angeles"]
                                 [?y :lahman/teamID   ?team]] (db conn))
                        => #{["NYN"] ["LAN"] ["SFN"] ["NYA"]}
+        ; Name of the NYM is New York Mets
+        (q '[:find ?name :where [?x :lahman/franchName ?name]
+                                [?x :lahman/franchID "NYM"]] (db conn))
+                       => #{["New York Mets"]}
+        ; FranchID of NYN is NYM
+        (q '[:find ?franchID :where [?x :lahman/teamID "NYN"]
+                                    [?x :lahman/franchID ?franchID]] (db conn))
+                       => #{["NYM"]}
+        ; Name of NYN is New York Mets
+        (q '[:find ?name :where [?x :lahman/teamID "NYN"]
+                                    [?x :lahman/name ?name]] (db conn))
+                       => #{["New York Mets"]}
         ; Daryll played for the New York Mets
-        (q '[:find ?franchName :where [?x :lahman/playerID "strawda01"]
-                                      [?x :lahman/teamID ?team]
-                                      [?y :lahman/teamID ?team]
-                                      [?y :lahman/franchID ?franchid]
-                                      [?z :lahman/franchID ?franchid]
-                                      [?z :lahman/franchName ?franchName]] (db conn))
-                       => (contains ["New York Mets"])
+        (q '[:find ?name :where [?x :lahman/playerID "strawda01"]
+                                [?x :lahman/teamID ?team]
+                                [?y :lahman/teamID ?team]
+                                [?y :lahman/name ?name]] (db conn))
+                       =>  #{["New York Mets"]}
       ))
