94de364c202ad8538b16f24939334800b8927ec4
John Jacobsen <john@mail.npxdesigns.com>
2013-08-10 08:36:53 -0500

    Fix references to resources/lahman2012/Master.csv (fixes #13)

--- protosite/src/protosite/lahman_load.clj
+++ protosite/src/protosite/lahman_load.clj
@@ -5,6 +5,7 @@
   (:use [datomic.api :only [db q] :as d])
   (:gen-class))
 
+
 (def filenames ["resources/lahman2012/Master.csv"
                 "resources/lahman2012/Batting.csv"
                 "resources/lahman2012/TeamsFranchises.csv"])
@@ -17,12 +18,13 @@
     (let [conn (d/connect uri)]
            (d/transact conn schema)
            (println "Added Schema")
-           (doseq [fact facts]
+           (doseq [fact (facts)]
              (d/transact conn [fact]))
            (println (format "Added %d facts" (count facts)))
            (d/request-index conn)
            (println "Requesting index"))
     (println "Database already up")))
+
 
 (defn -main []
   (populate db-uri)
