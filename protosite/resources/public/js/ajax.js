a3d41521329315c58648755f7beff0b3785590cf
Matthew Rocklin <mrocklin@gmail.com>
2013-08-11 07:41:46 -0500

    auto-complete on full team name

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -6,43 +6,42 @@ $( document ).ready( function() {
 
     oTable = $('#example').dataTable({
         "bProcessing": true,
-        "sAjaxSource": "/team/NYN/1990/",
-        "iDisplayLength": 25,
-		"fnServerData": function ( sUrl, aoData, fnCallback, oSettings ) {
-			oSettings.jqXHR = $.ajax( {
-				"url":  sUrl,
-				"data": aoData,
-				"success": function (json) {
-					if ( json.sError ) {
-						oSettings.oApi._fnLog( oSettings, 0, json.sError );
-					}
+           "sAjaxSource": "/team/NYN/1990/",
+           "iDisplayLength": 25,
+           "fnServerData": function ( sUrl, aoData, fnCallback, oSettings ) {
+               oSettings.jqXHR = $.ajax( {
+                   "url":  sUrl,
+               "data": aoData,
+               "success": function (json) {
+                   if ( json.sError ) {
+                       oSettings.oApi._fnLog( oSettings, 0, json.sError );
+                   }
 
-					$(oSettings.oInstance).trigger('xhr', [oSettings, json]);
-					fnCallback( json );
-				},
-				"dataType": "json",
-				"cache": true,
-				"type": "GET",
-				"error": function (xhr, error, thrown) {
-					if ( error == "parsererror" ) {
-						oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
-							"server could not be parsed. This is caused by a JSON formatting error." );
-					}
-				}
-			} );
-		},
+                   $(oSettings.oInstance).trigger('xhr', [oSettings, json]);
+                   fnCallback( json );
+               },
+               "dataType": "json",
+               "cache": true,
+               "type": "GET",
+               "error": function (xhr, error, thrown) {
+                   if ( error == "parsererror" ) {
+                       oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
+                           "server could not be parsed. This is caused by a JSON formatting error." );
+                   }
+               }
+               } );
+           },
     });
-    console.log(oTable);
 
     function updateDataTable(tbl, team, year){
+        team = teamMap[team];
         var url = "/team/" + team + "/" + year + "/";
-        console.log(url);
         tbl.fnReloadAjax(url);
     }
 
     $("#year").bind('keypress', function(e) {
         var year = $("#year").val() + String.fromCharCode(e.keyCode);
-        var team = $("#team").val();
+        var team = $("#team").val()
         if(year.length == 4){
             updateDataTable(oTable, team, year);
         }
@@ -51,18 +50,19 @@ $( document ).ready( function() {
     $("#team").bind('keypress', function(e) {
         var team = $("#team").val() + String.fromCharCode(e.keyCode);
         var year = $("#year").val() ;
-        if(team.length == 3){
+        if(contains(teamNames, team)){
             updateDataTable(oTable, team, year);
         }
     });
 
     $.ajax({
-        url: "/teamids/",
+        url: "/teamnames/",
         success: function(data, aStatus, dummy){
+            teamMap = data;
+            teamNames = keys(data);
             $( "#team" ).autocomplete({
-                source: data
+                source: teamNames
             });},
         dataType: "json"});
 
 });
-
