010969183e1a412536409a300275a6e1a5d44cf1
Matthew Rocklin <mrocklin@gmail.com>
2013-08-12 18:49:24 -0500

    move javascript functions out of ready event

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -1,38 +1,34 @@
+function urlFrom(team, year){
+    return "/team/" + team + "/" + year + "/";
+}
 
+function loadDataTable(team, year){
+    $.ajax({
+        url: urlFrom(team, year),
+        success: function(data, aStatus, dummy){
+            oTable = $('#example').dataTable({
+                "bProcessing": true,
+            "aoColumns": data.aoColumns,
+            "aaData": data.aaData,
+            "iDisplayLength": 25,
+            });
+        },
+        dataType: "json"});
+}
 
+function updateDataTable(tbl, team, year){
+    var team = teamMap[team];
+    $.ajax({
+        url: urlFrom(team, year),
+        success: function(data, aStatus, dummy){
+            oTable.fnClearTable();
+            oTable.fnAddData(data.aaData);
+        },
+    dataType: "json"});
+}
 
 
 $( document ).ready( function() {
-
-
-    function urlFrom(team, year){
-        return "/team/" + team + "/" + year + "/";
-    }
-
-    function loadDataTable(team, year){
-        $.ajax({
-            url: urlFrom(team, year),
-            success: function(data, aStatus, dummy){
-                oTable = $('#example').dataTable({
-                    "bProcessing": true,
-                "aoColumns": data.aoColumns,
-                "aaData": data.aaData,
-                "iDisplayLength": 25,
-                });
-            },
-            dataType: "json"});
-    }
-
-    function updateDataTable(tbl, team, year){
-        var team = teamMap[team];
-        $.ajax({
-            url: urlFrom(team, year),
-            success: function(data, aStatus, dummy){
-                oTable.fnClearTable();
-                oTable.fnAddData(data.aaData);
-            },
-        dataType: "json"});
-    }
 
     loadDataTable("NYN", 1990);
 
