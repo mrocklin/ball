ff81f5db31c073ac179364a98404ce1e0ec016e7
Matthew Rocklin <mrocklin@gmail.com>
2013-08-12 18:10:36 -0500

    Batting attributes are now separate from HTML

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -4,39 +4,30 @@
 
 $( document ).ready( function() {
 
-    oTable = $('#example').dataTable({
-        "bProcessing": true,
-           "sAjaxSource": "/team/NYN/1990/",
-           "iDisplayLength": 25,
-           "fnServerData": function ( sUrl, aoData, fnCallback, oSettings ) {
-               oSettings.jqXHR = $.ajax( {
-                   "url":  sUrl,
-               "data": aoData,
-               "success": function (json) {
-                   if ( json.sError ) {
-                       oSettings.oApi._fnLog( oSettings, 0, json.sError );
-                   }
+    // Load Data Table
+    $.ajax({
+        url: "/team/NYN/1990/",
+        success: function(data, aStatus, dummy){
+            oTable = $('#example').dataTable({
+                "bProcessing": true,
+            "aoColumns": data.aoColumns,
+            "aaData": data.aaData,
+            "iDisplayLength": 25,
+            });
+        },
+        dataType: "json"});
 
-                   $(oSettings.oInstance).trigger('xhr', [oSettings, json]);
-                   fnCallback( json );
-               },
-               "dataType": "json",
-               "cache": true,
-               "type": "GET",
-               "error": function (xhr, error, thrown) {
-                   if ( error == "parsererror" ) {
-                       oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
-                           "server could not be parsed. This is caused by a JSON formatting error." );
-                   }
-               }
-               } );
-           },
-    });
 
     function updateDataTable(tbl, team, year){
         team = teamMap[team];
         var url = "/team/" + team + "/" + year + "/";
-        tbl.fnReloadAjax(url);
+        $.ajax({
+            url: url,
+            success: function(data, aStatus, dummy){
+                oTable.fnClearTable();
+                oTable.fnAddData(data.aaData);
+            },
+        dataType: "json"});
     }
 
     $("#year").bind('keypress', function(e) {
@@ -55,6 +46,7 @@ $( document ).ready( function() {
         }
     });
 
+    // Set up auto-complete
     $.ajax({
         url: "/teamnames/",
         success: function(data, aStatus, dummy){
@@ -65,5 +57,4 @@ $( document ).ready( function() {
             });
         },
         dataType: "json"});
-
 });
