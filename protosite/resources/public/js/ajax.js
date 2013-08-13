e0233a3ddca467a4dff1407969cc4c961fcd2813
Matthew Rocklin <mrocklin@gmail.com>
2013-08-12 19:36:21 -0500

    pull out common dataTables function to separate file

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -2,31 +2,6 @@ function urlFrom(team, year){
     return "/team/" + team + "/" + year + "/";
 }
 
-function loadDataTable(url){
-    $.ajax({
-        url: url,
-        success: function(data, aStatus, dummy){
-            oTable = $('#example').dataTable({
-                "bProcessing": true,
-            "aoColumns": data.aoColumns,
-            "aaData": data.aaData,
-            "iDisplayLength": 25,
-            });
-        },
-        dataType: "json"});
-}
-
-function updateDataTable(tbl, url){
-    $.ajax({
-        url: url,
-        success: function(data, aStatus, dummy){
-            oTable.fnClearTable();
-            oTable.fnAddData(data.aaData);
-        },
-    dataType: "json"});
-}
-
-
 $( document ).ready( function() {
 
     loadDataTable(urlFrom("NYN", 1990));
