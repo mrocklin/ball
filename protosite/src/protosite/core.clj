a9e6b6d23c58657e003139e18cd3cad213c07d31
Matthew Rocklin <mrocklin@gmail.com>
2013-08-12 19:13:22 -0500

    Route for player stats - example use of no-join

--- protosite/src/protosite/core.clj
+++ protosite/src/protosite/core.clj
@@ -18,6 +18,9 @@
   (GET "/query/" request (let [want (get-in request [:params "want"])
                                constraints (get-in request [:params "constraints"])]
           (json/write-str (basic-query (d/connect db-uri) want constraints))))
+  (GET ["/player/:pid/" :pid #"\w*"] [pid]
+       (json/write-str (sort-by first (no-join-query (d/connect db-uri)
+            (concat ["yearID" "teamID"] batting-attrs) [["playerID" pid]]))))
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
 
