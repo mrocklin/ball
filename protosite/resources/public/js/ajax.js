6b857387eb8839e4def4b494e7dec975fe3bd0e0
John Jacobsen <john@mail.npxdesigns.com>
2013-08-14 22:41:49 -0500

    Issue 22: histograms for data
    This commit for #22 provides a single histogram while clicking on a statistic.
    
    It should provide enough of an example to get us started.

--- protosite/resources/public/js/ajax.js
+++ protosite/resources/public/js/ajax.js
@@ -24,11 +24,25 @@ function setUpClickAction(){
             url: "/year-attribute/"+year+"/"+attribute+"/",
             dataType: "json",
             success: function(data, sStatus, dummy){
-                        var bars = bins(data.data, 20);
-                        $("#yeardata").html(
-                        "BarCenters: "+bars.centers.join(', ') + "    " +
-                        "BarHeights: "+bars.heights.join(', '));
-                        tmp = data.data;
+                var bars = bins(data.data, 20);
+                $("#yeardata").html("");
+                i3d3.plot({data: [{type: "bars",
+                                   bins: bars.heights,
+                                   color: "blue",
+                                   range: [_.min(data.data), 
+                                           _.max(data.data)]
+                                  }],
+                           div: "yeardata",
+                           size: [500, 200],
+                           xlabel: "x axis",
+                           ylabel: "y axis",
+                           extras: []
+                          });
+                
+                        // $("#yeardata").html(
+                        // "BarCenters: "+bars.centers.join(', ') + "    " +
+                        // "BarHeights: "+bars.heights.join(', '));
+                        // tmp = data.data;
                      }
         });
     });
