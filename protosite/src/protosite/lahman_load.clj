549674b10d3617ac7435baf402a55062307c07d1
Matthew Rocklin <mrocklin@gmail.com>
2013-08-18 10:52:04 -0500

    Batch transactions.  Also, use async

--- protosite/src/protosite/lahman_load.clj
+++ protosite/src/protosite/lahman_load.clj
@@ -11,9 +11,17 @@
                 "resources/lahman2012/Teams.csv"
                 "resources/lahman2012/TeamsFranchises.csv"])
 
-(defn get-facts [] (->> filenames
-                        (map datomic-facts-from-filename)
-                        flatten))
+;; Stuart Halloway
+;; https://groups.google.com/forum/#!msg/datomic/ZethRt6dqxs/GceIbj53_P8J
+(defn transact-pbatch
+    "Submit txes in batches of size batch-size, default is 100"
+    ([conn txes] (transact-pbatch conn txes 100))
+    ([conn txes batch-size]
+          (->> (partition-all batch-size txes)
+                      (pmap #(d/transact-async conn (mapcat identity %)))
+                      (map deref)
+                      dorun)
+          :ok))
 
 
 (defn count-with-action
@@ -31,6 +39,15 @@
          (f (first s))
          (recur f (rest s) (inc' n))))))
 
+
+(defn get-transactions []
+  (for [f
+    (->> filenames
+      (map datomic-facts-from-filename)
+      flatten)]
+    [f]))
+
+
 (defn populate [uri]
   (println "Trying to create DB...")
   (if-not (d/create-database uri)
@@ -40,9 +57,8 @@
       (let [conn (d/connect uri)]
         (d/transact conn schema)
         (println "Added Schema; processing facts...")
-        (let [facts (get-facts)
-              n (count-with-action #(d/transact conn [%]) facts)]
-          (println (format "Processed %d facts" n)))
+        (transact-pbatch conn (get-transactions))
+        (println "Processed facts")
         (println "Requesting index")
         (d/request-index conn)
         (println "Shutting down")
